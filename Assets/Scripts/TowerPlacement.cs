f807c48092d867a73adc5952bc8b993291a15872
xzars-git <xzars@upi.edu>
2021-09-19 21:57:37 +0700

    first commit

---
+++ Assets/Scripts/TowerPlacement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+
+
+public class TowerPlacement : MonoBehaviour
+
+{
+
+    private Tower _placedTower;
+
+
+
+    // Fungsi yang terpanggil sekali ketika ada object Rigidbody yang menyentuh area collider
+    // Kebalikan dari OnTriggerEnter2D, fungsi ini terpanggil sekali ketika object tersebut meninggalkan area collider
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (_placedTower != null && _placedTower.IsPlaced && !_placedTower.gameObject.activeSelf)
+        {
+            _placedTower = null;
+        }
+
+        if (_placedTower == null)
+        {
+            Tower tower = collision.GetComponent<Tower>();
+            if (tower != null)
+            {
+                tower.SetPlacePosition(transform.position);
+                _placedTower = tower;
+            }
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (_placedTower == null) return;
+
+        if (!_placedTower.IsPlaced)
+        {
+            _placedTower.SetPlacePosition(null);
+            _placedTower = null;
+        }
+
+    }
+
+}
